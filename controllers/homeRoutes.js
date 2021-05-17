de87f11aa0f81181ad584b5e632075fc68f7bc0d
Kelly Vaden <klvaden@gmail.com>
2021-05-17 15:57:25 -0400

    installed Handlebars and configured it as the app's template engine; refactored homepage HTML into Handlebars partials, set up GET route to point to homepage.handlebars with hard-coded data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,20 +2,28 @@ const router = require('express').Router();
 // require necessary Models
 // require withAuth
 
+// router.get('/', async (req, res) => {
+//     try {
+//         // Get all posts
+//         const postData = await Post.findAll();
+    
+//         // Serialize data so the template can read it
+//         const posts = postData.map((post) => post.get({ plain: true }));
+    
+//         // Pass serialized data and session flag into template
+//         res.render('homepage');
+//             //'homepage', { 
+//             // posts, 
+//             // logged_in: req.session.logged_in });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
 router.get('/', async (req, res) => {
-    try {
-        // Get all posts
-        const postData = await Post.findAll();
-    
-        // Serialize data so the template can read it
-        const posts = postData.map((post) => post.get({ plain: true }));
-    
-        // Pass serialized data and session flag into template
-        // res.render('homepage', { 
-        //     posts, 
-        //     // logged_in: req.session.logged_in 
-        // });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    // Add a comment describing the purpose of the render method
+    // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
+    res.render('homepage');
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 // Require dependencies
+const path = require('path');
 const express = require('express');
+// const session = require('express-session');
+const exphbs = require('express-handlebars');
+// const routes = require('./controllers/homeRoutes'); // TODO: point to controllers/index.js when created
+// const helpers = require('./utils/helpers);
 
-// Import Sequelize connection
+// Import Sequelize connection from config folder
 const sequelize = require('./config/connection');
+// session.Store
 
 // Set up an Express app
 const app = express();
 const PORT = process.env.PORT || 1001;
 
-// Start server and sequelize connection
+// Set up a Handlebars.js engine
+const hbs = exphbs.create({});
+
+// Configure Express to use Handlebars as the template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/homeRoutes')); // TODO: change this to variable in line 6
+
+// Configure Express to work with JSON data in POST requests
+
+// Set up and use a session ...
+
+// Start server and database connection via Sequelize
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
 });
